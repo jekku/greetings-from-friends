b449d30e115f8daf9933384a6f9d1282db5e3e2e
Jekri Preclaro Orlina <jekku@users.noreply.github.com>
2017-05-29 14:25:06 +0800

    Revert "Updates:"

--- js/index.js
+++ js/index.js
@@ -1,56 +1,28 @@
 var maxElements = 25;
 var colors = ['#F25F5C', '#FFE066', '#247BAO', '#70C1B3'];
 var bg = '#50514F';
-var messages = [
-    {
-        message: '>:3<',
-        sender: 'BurnzZ'
-    },
-    {
-        message: 'BAGUL',
-        sender: 'adin234'
+var display = '>:3<'.split('');
+
+var createElements = (function() {
+  var sections = Array.prototype.slice.call(document.querySelectorAll('section'));
+  sections.forEach( function (sectionEl, l) {
+    var letter = getLetter(display[l]);
+    for (var i = 0; i < maxElements; i++) {
+      var el = document.createElement('div');
+      if (letter[i] === 1) {
+        el.style.background = colors[anime.random(0, 3)];
+      }
+      else {
+        el.style.opacity = 0.0;
+      }
+      sectionEl.appendChild(el);
     }
-];
-
-var generateSections = function (message) {
-    return message.split('').map(function (letter) {
-        var sectionEl = document.createElement('section');
-        var letter = getLetter(letter);
-        for (var i = 0; i < maxElements; i++) {
-            var el = document.createElement('span');
-            if (letter[i] === 1) {
-                el.style.background = colors[anime.random(0, 3)];
-            } else {
-                el.style.opacity = 0.0;
-            }
-
-            sectionEl.appendChild(el);
-        }
-
-        return sectionEl;
-    });
-};
-
-messages.forEach(function (messageObj) {
-    var div = document.createElement('div');
-    var lettersDiv = document.createElement('div');
-
-    lettersDiv.className = 'character';
-    Array.prototype.forEach.call(generateSections(messageObj.message), function (section) {
-        lettersDiv.appendChild(section);
-    });
-
-    var sender = document.createElement('p');
-    sender.innerHTML = '- ' + messageObj.sender;
-
-    div.appendChild(lettersDiv);
-    div.appendChild(sender);
-
-    document.body.appendChild(div);
-});
+    document.body.appendChild(sectionEl);
+  });
+})();
 
 anime({
-  targets: 'span',
+  targets: 'div',
   translateX: function() { return anime.random(-6, 6) + 'rem'; },
   translateY: function() { return anime.random(-6, 6) + 'rem'; },
   scale: function() { return anime.random(10, 20) / 10; },
